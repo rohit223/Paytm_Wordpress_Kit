840e2d3f654727576914d52819efd4d8f012ae88
Abhishek Goyal <abhishek.goyal@one97.delhi.net>
2017-04-12 12:59:48 +0530

    Status API Update

--- Wordpress_v3to4.x_compatible/paytm-donation/paytm-donation.php
+++ Wordpress_v3to4.x_compatible/paytm-donation/paytm-donation.php
@@ -491,14 +491,18 @@ function paytm_donation_response(){
 				// Create an array having all required parameters for status query.
 				$requestParamList = array("MID" => $paytm_merchant_id , "ORDERID" => $_POST['ORDERID']);
 				
+				$StatusCheckSum = getChecksumFromArray($requestParamList, $paytm_merchant_key);
+							
+				$requestParamList['CHECKSUMHASH'] = $StatusCheckSum;
+				
 				// Call the PG's getTxnStatus() function for verifying the transaction status.
 				
-				$check_status_url = 'https://pguat.paytm.com/oltp/HANDLER_INTERNAL/TXNSTATUS';
+				$check_status_url = 'https://pguat.paytm.com/oltp/HANDLER_INTERNAL/getTxnStatus';
 				if($paytm_mode == 'LIVE')
 				{
-					$check_status_url = 'https://secure.paytm.in/oltp/HANDLER_INTERNAL/TXNSTATUS';
+					$check_status_url = 'https://secure.paytm.in/oltp/HANDLER_INTERNAL/getTxnStatus';
 				}
-				$responseParamList = callAPI($check_status_url, $requestParamList);
+				$responseParamList = callNewAPI($check_status_url, $requestParamList);
 				if($responseParamList['STATUS']=='TXN_SUCCESS' && $responseParamList['TXNAMOUNT']==$_POST['TXNAMOUNT'])
 				{
 					$wpdb->query($wpdb->prepare("UPDATE FROM " . $wpdb->prefix . "paytm_donation WHERE id = %d", $_POST['ORDERID']));
